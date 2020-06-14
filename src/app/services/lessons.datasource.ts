82fe2b2c71c446ff29a80481694178f2cdafc2a6
baberarjumand <baber.arjumand@gmail.com>
2020-06-14 17:38:36 +0300

    completed video 3.6

--- src/app/services/lessons.datasource.ts
+++ src/app/services/lessons.datasource.ts
@@ -1,14 +1,28 @@
-import { DataSource, CollectionViewer } from "@angular/cdk/collections";
-import { Observable } from "rxjs";
-import { Lesson } from "../model/lesson";
-import { CoursesService } from "./courses.service";
+import { DataSource, CollectionViewer } from '@angular/cdk/collections';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { Lesson } from '../model/lesson';
+import { CoursesService } from './courses.service';
 
 export class LessonsDataSource implements DataSource<Lesson> {
+  private lessonsSubject = new BehaviorSubject<Lesson[]>([]);
+
   constructor(private coursesService: CoursesService) {}
 
   connect(collectionViewer: CollectionViewer): Observable<Lesson[]> {
-    return undefined;
+    return this.lessonsSubject.asObservable();
   }
 
   disconnect(collectionViewer: CollectionViewer): void {}
+
+  loadLessons(
+    courseId: number,
+    filter: string,
+    sortDirection: string,
+    pageIndex: number,
+    pageSize: number
+  ) {
+    this.coursesService
+      .findLessons(courseId, filter, sortDirection, pageIndex, pageSize)
+      .subscribe((lessons) => this.lessonsSubject.next(lessons));
+  }
 }

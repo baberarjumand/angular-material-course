e506e819a621c12acc3df68fde1379319d72b209
baberarjumand <baber.arjumand@gmail.com>
2020-06-14 16:03:27 +0300

    completed video 2.5

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Course } from '../model/course';
 
 @Component({
-  selector: "courses-card-list",
+  selector: 'courses-card-list',
   templateUrl: './courses-card-list.component.html',
   styleUrls: ['./courses-card-list.component.scss'],
 })

9a082db1dde7cc6f18d8bed88f8ee5d26764f5a2
baberarjumand <baber.arjumand@gmail.com>
2020-06-15 08:54:04 +0300

    completed video 3.12

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -4,27 +4,27 @@ import {
   ElementRef,
   OnInit,
   ViewChild,
-} from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { Course } from '../model/course';
-import { CoursesService } from '../services/courses.service';
+} from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { Course } from "../model/course";
+import { CoursesService } from "../services/courses.service";
 import {
   debounceTime,
   distinctUntilChanged,
   startWith,
   tap,
   timeout,
-} from 'rxjs/operators';
-import { merge, fromEvent } from 'rxjs';
-import { MatTableDataSource } from '@angular/material/table';
-import { LessonsDataSource } from '../services/lessons.datasource';
+} from "rxjs/operators";
+import { merge, fromEvent } from "rxjs";
+import { MatTableDataSource } from "@angular/material/table";
+import { LessonsDataSource } from "../services/lessons.datasource";
 
 @Component({
   selector: "course",
-  templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css'],
+  templateUrl: "./course.component.html",
+  styleUrls: ["./course.component.css"],
 })
 export class CourseComponent implements OnInit, AfterViewInit {
   course: Course;
@@ -32,9 +32,11 @@ export class CourseComponent implements OnInit, AfterViewInit {
   //   dataSource = new MatTableDataSource([]);
   dataSource: LessonsDataSource;
 
-  displayColumns = ['seqNo', 'description', 'duration'];
+  displayColumns = ["seqNo", "description", "duration"];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  @ViewChild(MatSort) matSort: MatSort;
 
   constructor(
     private route: ActivatedRoute,
@@ -42,7 +44,7 @@ export class CourseComponent implements OnInit, AfterViewInit {
   ) {}
 
   ngOnInit() {
-    this.course = this.route.snapshot.data['course'];
+    this.course = this.route.snapshot.data["course"];
 
     // this.coursesService
     //   .findAllCourseLessons(this.course.id)
@@ -52,18 +54,22 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
     this.dataSource = new LessonsDataSource(this.coursesService);
 
-    this.dataSource.loadLessons(this.course.id, '', 'asc', 0, 3);
+    this.dataSource.loadLessons(this.course.id, "", "asc", 0, 3);
   }
 
   ngAfterViewInit() {
-    this.paginator.page
+    // any time sort order changes, results are set to show page 0
+    this.matSort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
+
+    merge(this.matSort.sortChange, this.paginator.page)
       .pipe(
         startWith(null),
         tap(() => {
           this.dataSource.loadLessons(
             this.course.id,
-            '',
-            'asc',
+            "",
+            // 'asc',
+            this.matSort.direction,
             this.paginator.pageIndex,
             this.paginator.pageSize
           );

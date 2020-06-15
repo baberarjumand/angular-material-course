5fb156eb38b231be2554b95aef78a0ceeb89c3fd
baberarjumand <baber.arjumand@gmail.com>
2020-06-15 09:29:01 +0300

    completed video 4.2

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,27 +1,31 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
-import {Course} from "../model/course";
-import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
-import {CourseDialogComponent} from "../course-dialog/course-dialog.component";
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Course } from '../model/course';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { CourseDialogComponent } from '../course-dialog/course-dialog.component';
 
 @Component({
-    selector: 'courses-card-list',
-    templateUrl: './courses-card-list.component.html',
-    styleUrls: ['./courses-card-list.component.css']
+  selector: "courses-card-list",
+  templateUrl: './courses-card-list.component.html',
+  styleUrls: ['./courses-card-list.component.css'],
 })
 export class CoursesCardListComponent implements OnInit {
+  @Input()
+  courses: Course[];
 
-    @Input()
-    courses: Course[];
+  constructor(private dialog: MatDialog) {}
 
-    constructor() {
-    }
+  ngOnInit() {}
 
-    ngOnInit() {
+  editCourse({ description, longDescription, category }: Course) {
+    const dialogConfig = new MatDialogConfig();
 
-    }
+    dialogConfig.disableClose = true;
+    dialogConfig.data = {
+      description,
+      longDescription,
+      category,
+    };
 
-    editCourse({description, longDescription, category}:Course) {
-
-    }
-
+    this.dialog.open(CourseDialogComponent, dialogConfig);
+  }
 }

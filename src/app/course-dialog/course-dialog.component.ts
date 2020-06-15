d8119eeb91197f589d5fcabeae0d75e6c1caebf5
baberarjumand <baber.arjumand@gmail.com>
2020-06-15 09:36:42 +0300

    completed video 4.3

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,34 +1,26 @@
-import {Component, Inject, OnInit, ViewEncapsulation} from '@angular/core';
+import { Component, Inject, OnInit, ViewEncapsulation } from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
-import * as moment from 'moment';
+import { Course } from "../model/course";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
+import * as moment from "moment";
 
 @Component({
-    selector: 'course-dialog',
-    templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css']
+  selector: "course-dialog",
+  templateUrl: "./course-dialog.component.html",
+  styleUrls: ["./course-dialog.component.css"],
 })
 export class CourseDialogComponent implements OnInit {
+  description: string;
 
+  constructor(
+    @Inject(MAT_DIALOG_DATA) { description, longDescription, category }: Course
+  ) {
+    this.description = description;
+  }
 
-    constructor() {
+  ngOnInit() {}
 
+  save() {}
 
-    }
-
-    ngOnInit() {
-
-    }
-
-
-    save() {
-
-
-    }
-
-    close() {
-
-    }
-
+  close() {}
 }
